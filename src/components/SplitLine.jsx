bff5b116547c9fcdc0492c15c2152396cf56fbb4
Emmancs <emmanuelwinfred245@gmail.com>
2026-09-09 15:57:49 +0530

    deletion

--- src/components/SplitLine.jsx
+++
@@ -1,7 +0,0 @@
-export default function SplitLine({ as: Tag = "span", className = "", children, ...props }) {
-  return (
-    <Tag className={`split-line ${className}`.trim()} {...props}>
-      <span>{children}</span>
-    </Tag>
-  );
-}
